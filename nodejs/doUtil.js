4c5def8ab6da647267322e228b5224b32c7ebbae
Kat Austria <k.austria@f5.com>
2019-05-14 17:37:13 -0700

    30-rollback-fails-functional-test - fix functional test scope, enable running individual describe hook - fix error object returned when checkDnsResolution fails - change gw value example in functional tests - change error code being overwritten by restWorker

--- nodejs/doUtil.js
+++ nodejs/doUtil.js
@@ -313,6 +313,7 @@ module.exports = {
                 dns.lookup(address, (error) => {
                     if (error) {
                         error.message = `Unable to resolve host ${address}: ${error.message}`;
+                        error.code = 424;
                         reject(error);
                         return;
                     }
@@ -321,6 +322,7 @@ module.exports = {
             } catch (error) {
                 // if DNS.resolve errors it throws an exception instead of rejecting
                 error.message = `Unable to resolve host ${address}: ${error.message}`;
+                error.code = 424;
                 reject(error);
             }
         });
